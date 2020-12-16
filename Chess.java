fa872855807aff679ef3311c113fe5485c2d292a
bradenjonsson00 <61176938+bradenjonsson00@users.noreply.github.com>
2020-12-16 10:07:17 -0600

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -3,8 +3,6 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Color.*;
 
-//idea: have a board object where each square contains a JButton and a Piece
-
 public class Chess {
     public static void main(String[] args) {
         new play();
